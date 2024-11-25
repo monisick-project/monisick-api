d3b9006f260b4d0112b5e783e896b5753a336922
iknaawa <133658549+iknaawa@users.noreply.github.com>
2024-11-25 13:45:56 +0700

    Update FoodModel.js

--- src/models/FoodModel.js
+++ src/models/FoodModel.js
@@ -42,7 +42,7 @@ const Food = sequelize.define("food", {
     monitoringPeriodId: {
         type: DataTypes.INTEGER,
         references: {
-            model: "monitoring_periods", // Nama tabel MonitoringPeriod
+            model: "monitoring_periods", 
             key: "id",
         },
         allowNull: false,
