82abec67e4e5e9b23dd22ab3db87bf52f6ef654f
iknaawa <133658549+iknaawa@users.noreply.github.com>
2024-11-25 13:10:16 +0700

    Update Homepage.js

--- src/controllers/Homepage.js
+++ src/controllers/Homepage.js
@@ -2,13 +2,13 @@ import MonitoringPeriod from "../models/MonitoringPeriodModel.js";
 
 // Endpoint untuk mengambil monitoring period milik user
 export const getHomepageData = async (req, res) => {
-    const userId = req.userId; // Mendapatkan userId dari token yang diterima di middleware
+    const userId = req.userId;
     try {
         const monitoringPeriods = await MonitoringPeriod.findAll({
             where: { user_id: userId },
-            attributes: ['id', 'monitoring_name', 'start_date', 'end_date', 'user_id'], // Hanya ambil data yang diperlukan
+            attributes: ['id', 'monitoring_name', 'start_date', 'end_date', 'user_id'], 
         });
-        res.json(monitoringPeriods); // Kirim data monitoring period ke frontend
+        res.json(monitoringPeriods); 
     } catch (error) {
         console.log(error);
         res.status(500).json({ msg: "Server error" });
