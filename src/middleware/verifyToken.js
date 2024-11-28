0e4ebdef737a5c91ca8c9752396fed6483d6508b
iknaawa <133658549+iknaawa@users.noreply.github.com>
2024-11-29 01:23:40 +0700

    Update verifyToken.js

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,9 +1,8 @@
 import jwt from "jsonwebtoken";
 
-
 export const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    console.log("Authorization Header: ", authHeader);  // Log header untuk debugging
+    console.log("Authorization Header: ", authHeader); 
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
